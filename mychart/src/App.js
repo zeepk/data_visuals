e8413e0c668523328db289e5e526b74979870693
Matthew Hughes <matthew.l.hughes@kp.org>
2019-08-29 15:57:25 -0700

    load from json

--- mychart/src/App.js
+++ mychart/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import ChartInfo from './chartInfo.json'
 import './App.css';
 import Chart from './components/Chart.js';
 
@@ -16,35 +16,36 @@ class App extends Component {
   }
 
   getChartData(){
-    //Ajax call here
-    this.setState({
-      chartData:{
-        labels: [
-          "Boston",
-          "Worcester",
-          "Springfield",
-          "Lowell",
-          "Cambridge",
-          "New Bedford"
-        ],
-        datasets: [
-          {
-            label: "Population",
-            data: [617594, 181045, 153060, 106519, 105162, 95072],
-            // backgroundColor:'green'
-            backgroundColor:[
-              'rgba(255, 99, 132, 0.6)',
-              'rgba(54, 162, 235, 0.6)',
-              'rgba(255, 206, 86, 0.6)',
-              'rgba(75, 192, 192, 0.6)',
-              'rgba(153, 102, 255, 0.6)',
-              'rgba(255, 159, 64, 0.6)',
-              'rgba(255, 99, 132, 0.6)'
-            ]
-          }
-        ]
-      }
-    });
+    // let theData = {
+    //   labels: [
+    //     "Boston",
+    //     "Worcester",
+    //     "Springfield",
+    //     "Lowell",
+    //     "Cambridge",
+    //     "New Bedford"
+    //   ],
+    //   datasets: [
+    //     {
+    //       label: "Population",
+    //       data: [617594, 181045, 153060, 106519, 105162, 95072],
+    //       // backgroundColor:'green'
+    //       backgroundColor:[
+    //         'rgba(255, 99, 132, 0.6)',
+    //         'rgba(54, 162, 235, 0.6)',
+    //         'rgba(255, 206, 86, 0.6)',
+    //         'rgba(75, 192, 192, 0.6)',
+    //         'rgba(153, 102, 255, 0.6)',
+    //         'rgba(255, 159, 64, 0.6)',
+    //         'rgba(255, 99, 132, 0.6)'
+    //       ]
+    //     }
+    //   ]
+    // }
+    var theJson = '{ "labels": [ "Boston", "Worcester", "Springfield", "Lowell", "Cambridge", "New Bedford" ], "datasets": [ { "label": "Population", "data": [ 617594, 181045, 153060, 106519, 105162, 95072 ], "backgroundColor": [ "rgba(255, 99, 132, 0.6)", "rgba(54, 162, 235, 0.6)", "rgba(255, 206, 86, 0.6)", "rgba(75, 192, 192, 0.6)", "rgba(153, 102, 255, 0.6)", "rgba(255, 159, 64, 0.6)", "rgba(255, 99, 132, 0.6)" ] } ] } ';
+    let theData = JSON.parse(theJson);
+
+    this.setState({chartData: theData});
   }
 
 
